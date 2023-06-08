8f1b9499f4738270acd9232357b9dadfddffa220
Каймакчи Александр <kaymakchiao@mail.ru>
2023-06-08 21:06:07 +0300

    Second commit

---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/AppActivityTest.kt
@@ -0,0 +1,91 @@
+package ru.iteco.fmhandroid.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import kotlinx.coroutines.withTimeout
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import ru.iteco.fmhandroid.R
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class AppActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(AppActivity::class.java)
+
+    @Test
+    fun appActivityTest() {
+        Thread.sleep(7000);
+        val textInputEditText = onView(withId(R.id.login_text_input_layout));
+        textInputEditText.check((matches(isDisplayed())))
+        textInputEditText.perform(replaceText("login2"), closeSoftKeyboard())
+
+        val textInputEditText2 = onView(withId(R.id.password_text_input_layout));
+        textInputEditText2.check((matches(isDisplayed())))
+        textInputEditText2.perform(replaceText("password2"), closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.enter_button), withText("�����"), withContentDescription("���������"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.RelativeLayout")),
+                        1
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val imageView = onView(
+            allOf(
+                withId(R.id.trademark_image_view),
+                withParent(
+                    allOf(
+                        withId(R.id.container_custom_app_bar_include_on_fragment_main),
+                        withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        imageView.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
